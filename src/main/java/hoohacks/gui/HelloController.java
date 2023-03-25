cbe1ef910b1e5435617d4b11f5f832e1c9e5847b
larissacybyk <jdj9ng@virginia.edu>
2023-03-25 14:47:38 -0400

    ?

--- src/main/java/hoohacks/gui/HelloController.java
+++ src/main/java/hoohacks/gui/HelloController.java
@@ -1,14 +1,30 @@
 package hoohacks.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Pane simulation;
+
+    @FXML
+    private Button goButton;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("idk!");
+        //goButton.setOnAction(e -> );
+    }
+    public void initialize(){
+        simulation.setVisible(false);
+    }
+    protected void onGoButton(){
+        //run simulation with file
+        simulation.setVisible(true);
     }
 }
